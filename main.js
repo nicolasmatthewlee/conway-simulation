d3826a3a45e8b340975349039279da3852e80f5c
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-11-07 23:16:33 -0800

    added simulation (currently incorrectly adjusts each square immediately after evaluation which influences the evaluation of subsequent items)

--- main.js
+++ main.js
@@ -21,6 +21,14 @@ class GridItem {
         this.value=!this.value;
     }
 
+    update() {
+        if (this.value==1) {
+            this.object.classList.add('active');
+        } else {
+            this.object.classList.remove('active');
+        }
+    }
+
 }
 
 class Grid {
@@ -34,7 +42,7 @@ class Grid {
         this.object.setAttribute('class','grid');
         this.object.style.gridTemplate = `repeat(${n},1fr) / repeat(${n},1fr)`;
 
-        // add Block objects to array
+        // add grid items to array
         for (let r=0;r<this.length;r++) {
 
             // must add array to array in for loop because fill passes the same object to all indices
@@ -49,7 +57,54 @@ class Grid {
             }
         }
     }
+
+    simulate() {
+
+        // for each item
+        for (let x=0;x<this.length;x++) {
+            for (let y=0;y<this.length;y++) {
+
+                // calculate sum of surrounding blocks (if they exist)
+                let sum = 0;
+                for (let dx of [-1,0,1]) {
+                    for (let dy of [-1,0,1]) {
+                        if (!(dx==0 && dy==0)) {
+                            try {
+                                sum+=this.items[x+dx][y+dy].value;
+                            } catch {}
+                            
+                        }
+                    }
+                }
+
+                // adjust value
+                if (this.items[x][y].value==1) {
+                    if (sum==2 || sum==3) {
+                        this.items[x][y].value=0;
+                        this.items[x][y].update();
+                    }
+                } else {
+                    if (sum==3) {
+                        this.items[x][y].value=1;
+                        this.items[x][y].update();
+                    }
+                }
+
+                // console.log(`${x},${y}:${sum}`)
+            }
+        }
+    }
 }
 
-let grid = new Grid(4);
+// create grid
+let grid = new Grid(3);
 body.appendChild(grid.object);
+
+// simulate every second
+// must call grid.simulate() so that `this` references grid
+//setInterval(()=>grid.simulate(),2000);
+
+let play_button = document.createElement('button');
+play_button.setAttribute('class','play-button');
+play_button.addEventListener('click',()=>grid.simulate())
+body.appendChild(play_button)
