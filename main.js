8abf0d2d0929194667c2307c84fb6320c3a9126c
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-11-07 22:19:14 -0800

    reworked grid to use GridItems

--- main.js
+++ main.js
@@ -2,43 +2,54 @@ body = document.querySelector('body');
 
 grid_object = document.createElement('div');
 
+class GridItem {
+
+    constructor() {
+
+        this.object = document.createElement('button');
+        this.object.setAttribute('class','grid-item');
+
+        // must bind to access this.value in this.toggle
+        this.object.addEventListener('click',this.toggle.bind(this));
+
+        this.value=0;
+
+    }
+
+    toggle() {
+        this.object.classList.toggle('active');
+        this.value=!this.value;
+    }
+
+}
+
 class Grid {
+
     constructor(n) {
         this.length=n;
-        this.values = Array(n).fill(Array(n).fill(0));
+
+        this.items = [];
 
         this.object = document.createElement('div');
         this.object.setAttribute('class','grid');
         this.object.style.gridTemplate = `repeat(${n},1fr) / repeat(${n},1fr)`;
-    }
 
-    display() {
-
-        // clear grid
-        this.object.innerHTML="";
-
-        // redraw
+        // add Block objects to array
         for (let r=0;r<this.length;r++) {
+
+            // must add array to array in for loop because fill passes the same object to all indices
+            // which resulted in each row taking on the values of the last row values set
+            this.items.push([]);
 
             for (let c=0;c<this.length;c++) {
 
-                let grid_item = document.createElement('div');
-                grid_item.setAttribute('class','grid-item');
-
-                if (this.values[r][c] == 1) {
-                    grid_item.classList.add('active');
-                }
-
-                this.object.appendChild(grid_item);
-
-                console.log(this.values[r][c]);
-
+                // must add to array in for loop because fill passes the same object to all indices
+                this.items[r].push(new GridItem());
+                this.object.appendChild(this.items[r][c].object);
             }
         }
     }
 }
 
-let grid = new Grid(5)
-grid.display()
-
+let grid = new Grid(4);
 body.appendChild(grid.object);
